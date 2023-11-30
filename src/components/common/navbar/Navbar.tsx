b44b9237edccec517c7a842e0ceec6cc2f6d2d8f
Ibtid <ibtid011@gmail.com>
2023-11-30 10:17:14 +0600

    change orientation of navbar

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import AddButton from '../buttons/add.button'
+import React from "react";
+import AddButton from "../buttons/add.button";
 
 const Navbar = () => {
   return (
     <header className="bg-gray-800 text-white p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <div className="text-2xl font-bold">Task Manager</div>
-          <div className="flex space-x-4">
-            <AddButton/>
-          </div>
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-2xl font-bold">Task Manager</div>
+        <div className="flex space-x-4">
+          <AddButton />
         </div>
-      </header>
-  )
-}
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
