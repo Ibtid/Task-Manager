3537d6584ff89c90f235c8a0f8ffc12aff4fd174
Ibtid <ibtid011@gmail.com>
2023-12-02 16:39:26 +0600

    remove eslint warnings

--- src/components/common/layout/Layout.tsx
+++ src/components/common/layout/Layout.tsx
@@ -1,21 +1,20 @@
-import React, { FC, Fragment } from "react";
+import { Fragment } from "react";
 import AddButton from "../buttons/add.button";
 import { useLocation, useNavigate } from "react-router-dom";
 import UiPaths from "../../../paths/uiPaths";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import BackButton from "../buttons/back.button";
 
 const Layout = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
-  console.log(pathname);
   return (
     <Fragment>
       <header className="bg-gray-800 text-white p-4">
         <div className="container mx-auto flex justify-between items-center">
           <div className="text-2xl font-bold">Task Manager</div>
           <div className="flex space-x-4">
-            {pathname == "/list" ? (
+            {pathname === "/list" ? (
               <AddButton onClick={() => navigate(UiPaths.AddTask)} />
             ) : (
               <BackButton onClick={() => navigate(UiPaths.TasksList)} />
